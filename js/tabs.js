ca42ce17a57661d29c8cb313055785cf41993997
Andrey <andrei200277@gmail.com>
2020-06-14 18:31:12 +0300

    release_v1

---
+++ js/tabs.js
@@ -0,0 +1,23 @@
+function tabs(tabsContentSelector, tabsParentSelector, activeClass) {
+    // TABS
+    let tabcontents = document.querySelectorAll(tabsContentSelector),
+        tabcontentButtons = document.querySelectorAll(tabsParentSelector);
+
+    function hideTabs(tab) {
+        for(let i = 0;i<tabcontentButtons.length;i++){
+            tabcontents[i].style.display = 'none';
+            tabcontentButtons[i].classList.remove(activeClass);
+        }
+        tabcontents[tab].style.display = 'block';
+        tabcontents[tab].classList.add('fade');
+        tabcontentButtons[tab].classList.add(activeClass);
+    }
+    hideTabs(0);
+    
+    for(let i = 0;i<tabcontentButtons.length;i++){
+        tabcontentButtons[i].addEventListener('click', () => {
+            hideTabs(i);
+        });
+    }
+}
+export default tabs;
