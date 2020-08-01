8f5c4a49a2335dafdf6043eb93de97e2a864d99a
IAndresI <andrei200277@gmail.com>
2020-08-02 01:16:45 +0300

    tabs_v2

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Tabs
-The content block is put in the place of the first attribute.
-The second attribute is the parent of the tabs.
-The third attribute is the activity class.
+The content block is put in property "content". (string)
+Tab buttons are put in property "button". (string)
+The buttons active class is put in property "activeClass". (string)
+The block that will appear if it doesn't find content is put in property "noContentAlert". (string)
+The animate is put in property "animate". (true/false)
+Default tab number is put in property "tabToShow". (number (starts at 0))
+
+# Usage Example
+
+tabs({
+    button: ".products__category-button",
+    content: ".product-item--tab",
+    activeClass: ".category--active",
+    noContentAlert: ".nothing-find",
+    animate: true,
+    tabToShow: 0
+  });
--- js/tabs.js
+++ js/tabs.js
@@ -1,21 +1,102 @@
-function tabs(tabsContentSelector, tabsParentSelector, activeClass) {
-    const tabcontents = document.querySelectorAll(tabsContentSelector),
-          tabcontentButtons = document.querySelectorAll(tabsParentSelector);
-    
-    function hideTabs(tab) {
-        for(let i = 0;i<tabcontentButtons.length;i++){
-            tabcontents[i].style.display = 'none';
-            tabcontentButtons[i].classList.remove(activeClass);
+function tabs({
+    button,
+    content,
+    activeClass,
+    animate,
+    noContentAlert,
+    tabToShow
+}) {
+    let tab_buttons = document.querySelectorAll(button),
+        tab_content = document.querySelectorAll(content),
+        therescontent = false,
+        nothingsFind = document.querySelector(noContentAlert);
+
+    if (tab_buttons[0] && tab_content) {
+        activeClass = activeClass.substring(1, activeClass.length);
+        let temp = 0;
+
+        function showTab(number) {
+            for (let i = 0; i < tab_content.length; i++) {
+                if (tab_content[i].getAttribute("data-content") == tab_buttons[number].getAttribute("data-tab")) {
+                    therescontent = true;
+                    tab_content[i].style.display = "block";
+                } else {
+                    tab_content[i].style.display = "none";
+                }
+                tab_buttons[number].classList.add(activeClass);
+                if (noContentAlert) {
+                    if (!therescontent) {
+                        nothingsFind.style.display = "block";
+                    } else {
+                        nothingsFind.style.display = "none";
+                    }
+                }
+            }
+            if (temp == 0) {
+                therescontent = false;
+                temp++;
+            }
         }
-        tabcontents[tab].style.display = 'block';
-        tabcontents[tab].classList.add('fade');
-        tabcontentButtons[tab].classList.add(activeClass);
-    }
-    hideTabs(0);
-    
-    for(let i = 0;i<tabcontentButtons.length;i++){
-        tabcontentButtons[i].addEventListener('click', () => {
-            hideTabs(i);
-        });
+
+        showTab(tabToShow);
+        for (let i = 0; i < tab_buttons.length; i++) {
+            tab_buttons[i].addEventListener("click", function (e) {
+                if (!e.target.classList.contains(activeClass)) {
+                    for (let i = 0; i < tab_buttons.length; i++) {
+                        tab_buttons[i].classList.remove(activeClass);
+                    }
+                    if (animate) {
+                        e.target.classList.add(activeClass);
+                        let lastTime = performance.now();
+                        for (let j = 0; j < tab_content.length; j++) {
+                            tab_content[j].style.transform = "scaleX(0) scaleY(0)";
+                            if (tab_content[j].getAttribute("data-content") === tab_buttons[i].getAttribute("data-tab")) therescontent = true;
+                            let animateID;
+
+                            function animation() {
+                                let currentTime = performance.now();
+                                if (currentTime - lastTime >= 200) {
+                                    tab_content[j].style.transition = "transform .2s";
+                                    tab_content[j].style.display = "none";
+                                    if (tab_content[j].getAttribute("data-content") === tab_buttons[i].getAttribute("data-tab")) {
+                                        tab_content[j].style.display = "block";
+                                        if (currentTime - lastTime >= 400) {
+                                            tab_content[j].style.transform = "scaleX(1) scaleY(1)";
+                                            cancelAnimationFrame(animateID);
+                                        }
+                                    }
+                                }
+                                if (currentTime - lastTime < 400) {
+                                    animateID = requestAnimationFrame(animation);
+                                }
+                            }
+                            animateID = requestAnimationFrame(animation);
+                        }
+                    } else {
+                        showTab(i);
+                    }
+                    if (noContentAlert) {
+                        if (!therescontent) {
+                            nothingsFind.style.display = "block";
+                        } else {
+                            nothingsFind.style.display = "none";
+                        }
+                    }
+                }
+                therescontent = false;
+            });
+        }
     }
 }
+export default tabs;
+
+// Usage Example
+
+// tabs({
+//     button: ".products__category-button",
+//     content: ".product-item--tab",
+//     activeClass: ".category--active",
+//     noContentAlert: ".nothing-find",
+//     animate: true,
+//     tabToShow: 0
+//   });
